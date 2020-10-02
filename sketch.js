5cfe329764c5e6851e695029e81a6548c081fa7b
greeshma1627 <68261180+greeshma1627@users.noreply.github.com>
2020-10-01 17:56:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var gameState=0;
+var playerCount;
+var form,player,game;
+var database;
+var allPlayers;
+ 
+function setup(){
+    //creating database from firebase database
+    database = firebase.database();
+
+    createCanvas(400,400);
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    if(playerCount===4){
+        game.update(1);
+
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
+
+ 
